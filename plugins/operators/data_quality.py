8fa8dbb3f17d8b4023bb5e66dfa0c9df99e277a9
ypatankar <yashashree.patankar@gmail.com>
2020-09-05 16:10:29 -0700

    updated the quality check dag to QA tables

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -15,31 +15,30 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
-                 table_list=[],
+                 qa_check_list=[],
                  *args, **kwargs):
-        """
-        This is the constructor for Data Quality Operator that initializes the necessary parameters
-        :param redshift_conn_id: redshift connection details
-        :param table_list: list of tables to be quality checked
-        :param args: context arguments
-        :param kwargs: keyword arguments
-        """
+
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.table_list = table_list
+        self.qa_check_list = qa_check_list
         self.redshift_conn_id = redshift_conn_id
 
     def execute(self, context):
-        """
-        This method executes the Data Quality task using the initialized parameters
-        :param context: context object
-        :return: None
-        """
+        error_count = 0
+        failing_tests = []
+        
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        for table in self.table_list:
-            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
-            if len(records) < 1 or len(records[0]) < 1:
-                raise ValueError(f"Data quality check failed. {table} returned no results")
-            num_records = records[0][0]
-            if num_records < 1:
-                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
-            logging.info(f"Data quality on table {table} check passed with {records[0][0]} records")
+        for check in self.qa_check_list:
+            sql = check.get('check_sql')
+            exp_result = check.get('expected_result')
+            records = redshift_hook.get_records(sql)[0]
+            
+            if exp_result != records[0]:
+                error_count += 1
+                failing_tests.append(sql)
+                
+        if error_count > 0:
+            self.log.info('Tests failed')
+            self.log.info(failing_tests)
+            raise ValueError('Data quality check failed') 
+        else:
+            self.log.info('Tests passed')
